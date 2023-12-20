746f2e46c12a0ad98193e8f2a76bd5b428061f54
Fabiano <fabiano.ti@unc.br>
2023-12-19 21:55:13 -0300

    modal delete funcionando

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 function App() {
   const [products, setProducts] = useState(null)
   const [searchTerm, setSearchTerm] = useState('');
+  const [showModal, setShowModal] = useState(false)
+  const [currentItem, setCurrentItem] = useState(null)
   const navigate = useNavigate();
 
   // Use effect
@@ -22,16 +24,28 @@ function App() {
     return navigate(`/update-product/${_id}`) 
   }
 
-  
+  const handleDelete = (_id) => {
+    setShowModal(true)
+    setCurrentItem(_id)
+    console.log(currentItem)
+  }
 
   const deleteProduct = async (_id) => {
-    /*
     const res = await axios.delete(`http://localhost:3000/api/products/${_id}`);
     const newProducts = [...products].filter((product) => {
       return product._id !== _id;
     })
     setProducts(newProducts);
-    */
+  }
+
+  const handleNo = () => {
+    setShowModal(false)
+  }
+
+  const handleYes = () => {
+    deleteProduct(currentItem)
+    setCurrentItem(null)
+    setShowModal(false)
   }
   
   const productsList = products ? products.map(product =>{
@@ -44,7 +58,7 @@ function App() {
         <button className='action-btn edit-btn' onClick={() => updateProduct(product._id)}>
           Editar
         </button>
-        <button id={product._id} className='action-btn delete-btn' onClick={() => deleteProduct(product._id)}>
+        <button id={product._id} className='action-btn delete-btn' onClick={(e) => handleDelete(product._id, e)}>
           Remover
         </button>
       </div>
@@ -79,6 +93,11 @@ function App() {
         <p><strong>Preço</strong></p>
         </div>
         {productsList}
+      </div>
+      <div className={showModal ? "modal" : "hidden"}>
+        Excluir produto?
+        <button onClick={handleNo}>Não</button>
+        <button onClick={handleYes}>Sim</button>
       </div>
     </div>
   );
